e38e1e2188b24c8669327054f4255c6ef4134c6d
Renan <renanlarosa@hotmail.com>
2022-07-27 23:13:02 -0300

    feat: strarted half color count formula

--- src/styles/solveFunciton.ts
+++ src/styles/solveFunciton.ts
@@ -11,16 +11,16 @@ import { IBoardColorCount, IColorCount, IGameBoard, IGridSize } from "../App";
 //    3.1- check if one is GC and other is SC -> At leat one cell in each extremety is SC color
 //    3.2- check if one color can only be in X line based on column -> that cell is that color
 //    3.3- check if one color can only be in X column based on line -> that cell is that color
-// 4- check if GCs fit between known cells and only in one place in L/C 
-// 5- check if SCs cant be next to known cells 
+// 4- check if GCs fit between known cells and only in one place in L/C
+// 5- check if SCs cant be next to known cells
 // 6- reduce count of each confirmed color mentally (temp count reduced)
 // 7- repeat?, 'excluding' known cells
-
 
 export function boardSolver(
   boardColorCount: IBoardColorCount,
   gameBoard: IGameBoard
 ) {
+  let tempBoard = gameBoard;
   for (let i = 0; i < gameBoard.lines.length; i++) {
     let line = gameBoard.lines[i];
     for (let j = 0; j < line.cells.length; j++) {
@@ -33,19 +33,31 @@ export function boardSolver(
         isOnlyColorInColumn(columnCount) || isOnlyColorInLine(lineCount);
       if (isOnlyColor) {
         cell.color = isOnlyColor.color;
-        console.log(isOnlyColor, i, j);
+        tempBoard.lines[i].cells[j].color = isOnlyColor.color;
       }
     }
   }
 }
 
+export function isBiggerThanHalfLine(
+  colorCount: IColorCount[],
+  gridSize: IGridSize = { columns: 5, lines: 5 }
+) {
+  for (let i = 0; i < colorCount.length; i++) {
+    let halfOfLine = gridSize.lines / 2;
+    if (colorCount[i].grouped && colorCount[i].count > halfOfLine && i) {
+      return colorCount[i];
+    }
+  }
+  return undefined;
+}
+
 export function isOnlyColorInLine(
   colorCount: IColorCount[],
-  gridSize?: IGridSize
+  gridSize: IGridSize = { columns: 5, lines: 5 }
 ) {
-  let gridSizeT = gridSize || { columns: 5, lines: 5 };
   for (let i = 0; i < colorCount.length; i++) {
-    if (colorCount[i].count === gridSizeT.lines) {
+    if (colorCount[i].count === gridSize.lines) {
       return colorCount[i];
     }
   }
@@ -54,12 +66,11 @@ export function isOnlyColorInLine(
 
 export function isOnlyColorInColumn(
   colorCount: IColorCount[],
-  gridSize?: IGridSize
+  gridSize: IGridSize = { columns: 5, lines: 5 }
 ) {
   let isOnlyColor = true;
-  let gridSizeT = gridSize || { columns: 5, lines: 5 };
   for (let i = 0; i < colorCount.length; i++) {
-    if (colorCount[i].count === gridSizeT.columns) {
+    if (colorCount[i].count === gridSize.columns) {
       return colorCount[i];
     }
   }
